be14d2809bad124205cd08d0c74c4a6069193fc6
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-30 23:19:11 +0100

    SidebarLink component

--- components/SidebarLink.js
+++ components/SidebarLink.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useRouter } from "next/router";
 
-const SidebarLink = () => {
+function SidebarLink({ Icon, text, active }) {
+  const router = useRouter();
   return (
-    <div>SidebarLink</div>
-  )
+    <div
+      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation ${
+        active && "font-bold"
+      }`}
+      onClick={() => active && router.push("/")}
+    >
+      <Icon className="h-7" />
+      <span className="hidden xl:inline">{text}</span>
+    </div>
+  );
 }
 
-export default SidebarLink
+export default SidebarLink;
