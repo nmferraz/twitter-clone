49864b5f88768a7b75abec07824705710373f32a
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-04-30 23:18:11 +0100

    global styles, basic structure for index.js and begin Sidebar component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import Head from "next/head";
+import Sidebar from "../components/Sidebar";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Head>
+        <title>Twitter Clone | by Nuno Ferraz</title>
+      </Head>
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+        {/*Feed */}
+        {/*Widgets */}
+
+        {/* Modal */}
+      </main>
+    </div>
+  );
 }
