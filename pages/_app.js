07167257424a477cdb37d771a5a98ed6d469922e
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-03 11:19:42 +0100

    fix pageProps

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
