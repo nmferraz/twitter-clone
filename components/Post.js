a50ab7d2782741f909a3b62ed9fc43c9122281bd
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-05 10:51:45 +0100

    progress

--- components/Post.js
+++ components/Post.js
@@ -75,7 +75,10 @@ function Post({ id, post, postPage }) {
     }
   };
   return (
-    <div className="p-3 flex cursor-pointer border-b border-gray-700">
+    <div
+      onClick={() => router.push(`/${id}`)}
+      className="p-3 flex cursor-pointer border-b border-gray-700"
+    >
       {!postPage && (
         <img
           src={post?.userImg}
