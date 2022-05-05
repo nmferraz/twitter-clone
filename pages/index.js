d495889d6e802f4a59388cf4c174a0d776710705
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-05-05 11:03:01 +0100

    Trending

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
 import Widgets from "../components/Widgets";
 
-export default function Home({ providers }) {
+export default function Home({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
 
@@ -34,12 +34,12 @@ export default function Home({ providers }) {
 }
 
 export async function getServerSideProps(context) {
-  const trendingResults = await fetch("https://jsonkeeper.com/b/").then(
-    (res) => res.json()
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then((res) =>
+    res.json()
   );
-  const followResults = await fetch("https://jsonkeeper.com/b/").then(
-    (res) => res.json()
-);
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then((res) =>
+    res.json()
+  );
   const providers = await getProviders();
   const session = await getSession(context);
 
@@ -52,4 +52,3 @@ export async function getServerSideProps(context) {
     },
   };
 }
-
